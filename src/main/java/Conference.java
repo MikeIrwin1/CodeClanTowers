7b93e390b4e1b877472d6919532b877737bfb835
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-02-13 10:46:48 +0000

    passing initial conference tests

--- src/main/java/Conference.java
+++ src/main/java/Conference.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Conference {
+
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> guests;
+
+    public Conference(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+        this.guests = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public int getNumberOfGuests(){
+        return this.guests.size();
+    }
+
+
 }
--- src/test/java/ConferenceTest.java
+++ src/test/java/ConferenceTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceTest {
+
+    private Conference conference;
+
+    @Before
+    public void before(){
+        conference = new Conference("MacLaren Suite", 40);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("MacLaren Suite", conference.getName());
+    }
+
+    @Test
+    public void canGetCapacity(){
+        assertEquals(40, conference.getCapacity());
+    }
+
+    @Test
+    public void canGetNumberOfGuests(){
+        assertEquals(0, conference.getNumberOfGuests());
+    }
+
+
 }
