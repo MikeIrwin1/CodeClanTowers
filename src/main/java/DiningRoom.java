b1116cc215d3c90e9630969045df43507d8addd3
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-02-13 10:56:32 +0000

    passing initial dining room

--- src/main/java/DiningRoom.java
+++ src/main/java/DiningRoom.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class DiningRoom {
+
+    private int capacity;
+    private String name;
+    private ArrayList<Guest> guests;
+
+    public DiningRoom(int capacity, String name){
+        this.capacity = capacity;
+        this.name = name;
+        this.guests = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getNumberOfGuests() {
+        return this.guests.size();
+    }
 }
--- src/test/java/DiningRoomTest.java
+++ src/test/java/DiningRoomTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DiningRoomTest {
+
+    private DiningRoom dining;
+
+    @Before
+    public void before(){
+        dining = new DiningRoom(40, "Mess Hall");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(40, dining.getCapacity());
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Mess Hall", dining.getName());
+    }
+
 }
